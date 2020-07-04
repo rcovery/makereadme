24104e16aa17f451b4c7aa789387d2cffc91c131
rcovery <ryanczpereira@gmail.com>
2020-07-04 20:13:24 -0300

    Initial project

---
+++ README.md
@@ -0,0 +1,21 @@
+<h1>~ make github readme ~</h1>This editor is a simple way to create a README.md file! <s>But there are still some bugs and it's in development</s>.
+
+Ruby:
+```ruby
+puts "You can enter codes!"
+```
+
+C:
+```c
+#include <stdio.h>
+
+int main()
+{
+    printf("Awesome!");
+    return(0);
+}
+```
+ 
+
+<b>Bold</b> text, <i>italic</i>, just select text and <b><i>click</i></b> on any <i>button</i>
+
---
+++ js/func.js
@@ -0,0 +1,41 @@
+var rt = document.getElementById("rtext");
+
+// Functions to apply effects
+function modtext(tag){
+	let selection = document.getSelection();
+	if (selection != ""){
+		document.execCommand('insertHTML', false, `<${tag}>` + document.getSelection() + `</${tag}>`);
+	}
+}
+
+function strong() {
+	if (document.getSelection() != ""){
+		document.execCommand('insertHTML', false, '<b><i>' + document.getSelection() + '</b></i>');
+	}
+}
+
+function icode(){
+	if (document.getSelection() != ""){
+		let language = window.prompt("(This editor does not work with colored syntaxes)\nWhich programming language do you want to use?");
+		if (language != ""){
+			let selection = document.getSelection();
+			console.log(selection.toString());
+			document.execCommand('insertHTML', false, `\n<code class="${language}icode">` + selection.toString().split("<").join("&lt;") + `</code>&nbsp;`);
+		}
+	}
+
+}
+
+// Function to download README.md
+function down(){
+	let txt = rt.innerHTML;
+
+	// Removing div and br tags
+	var newTxt = txt.split("<div>").join("").split("</div>").join("").split("<br>").join("\n");
+	// Replacing spaces
+	newTxt = newTxt.split("&nbsp;").join(" ");
+	// Replace code tags
+	newTxt = newTxt.split("<code ").join("\n```").split('class="').join("").split('icode">').join("\n").split("</code>").join("\n```\n");
+
+	document.getElementById("source").innerHTML = newTxt;
+}
